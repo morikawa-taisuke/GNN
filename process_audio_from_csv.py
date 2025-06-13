1f16e47f40e71d4a38e68489de358a802eff6f59
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-06-13 14:28:14 +0900

    main_mac.pyでのデータセットクラスの変更に伴い、train関数の引数を更新し、データの読み込み方法を修正しました。また、process_audio_from_csv.pyでのディレクトリ作成関数を修正し、ファイル名取得関数の呼び出しを統一しました。UGNNNet_DatasetClass.pyでは、ファイル名の一致確認を改善しました。

--- process_audio_from_csv.py
+++ process_audio_from_csv.py
@@ -45,7 +45,7 @@ def clean(csv_path, speech_dir, ir_dir, output_dir):
 
     df = pd.read_csv(csv_path)
     os.makedirs(output_dir, exist_ok=True)
-    my_func.exists_dir(output_dir)
+    my_func.make_dir(output_dir)
 
     for idx, row in tqdm(df.iterrows(), total=len(df)):
         # ファイル名抽出
@@ -65,7 +65,7 @@ def clean(csv_path, speech_dir, ir_dir, output_dir):
         speech_reverb = apply_ir(speech, speech_ir)
 
         # 保存
-        out_name = f"{my_func.get_fname(speech_file)[0]}.wav"
+        out_name = f"{my_func.get_file_name(speech_file)[0]}.wav"
         out_path = os.path.join(output_dir, out_name)
         save_wav(out_path, speech_reverb, sr)
         # print(f"Saved: {out_path}")
@@ -81,25 +81,29 @@ def noise_reverbe(csv_path, speech_dir, noise_dir, ir_dir, output_dir):
 
     df = pd.read_csv(csv_path)
     os.makedirs(output_dir, exist_ok=True)
-    my_func.exists_dir(output_dir)
+    my_func.make_dir(output_dir)
 
     for idx, row in tqdm(df.iterrows(), total=len(df)):
         # ファイル名抽出
         speech_file = os.path.basename(row['wav_path'])
         noise_file = os.path.basename(row['noise_path'])
-        speech_ir_file = os.path.basename(row['speech_IR'])
-        noise_ir_file = os.path.basename(row['noise_IR'])
-        snr = float(row['snr'])
+        # speech_ir_file = os.path.basename(row['speech_IR'])
+        # noise_ir_file = os.path.basename(row['noise_IR'])
+        # snr = float(row['snr'])
+        speech_ir_file = "050sec"
+        noise_ir_file = "050sec_000dig"
+        snr = 5.0
 
         # ファイルパス生成
-        speech_path = os.path.join(speech_dir, speech_file)
-        noise_path = os.path.join(noise_dir, noise_file)
+        speech_path = os.path.join(speech_dir, speech_file + ".wav")
+        noise_path = os.path.join(noise_dir, noise_file + ".wav")
         speech_ir_path = os.path.join(ir_dir, "speech", speech_ir_file + ".wav")
         noise_ir_path = os.path.join(ir_dir, "noise", noise_ir_file + ".wav")
 
         # 読み込み
         speech, sr = load_wav(speech_path)
-        noise, _ = load_wav(noise_path)
+        # noise, _ = load_wav(noise_path)
+        noise, _ = load_wav(noise_dir)
         speech_ir, _ = load_wav(speech_ir_path)
         noise_ir, _ = load_wav(noise_ir_path)
 
@@ -114,7 +118,7 @@ def noise_reverbe(csv_path, speech_dir, noise_dir, ir_dir, output_dir):
         mixed = mix_snr(speech_reverb, noise_reverb, snr)
 
         # 保存
-        out_name = f"{my_func.get_fname(speech_file)[0]}_{my_func.get_fname(noise_file)[0]}_{int(snr * 10):03}dB.wav"
+        out_name = f"{my_func.get_file_name(speech_file)[0]}_{my_func.get_file_name(noise_file)[0]}_{int(snr * 10):03}dB.wav"
         out_path = os.path.join(output_dir, out_name)
         save_wav(out_path, mixed, sr)
         # print(f"Saved: {out_path}")
@@ -129,7 +133,7 @@ def reverbe_only(csv_path, speech_dir, ir_dir, output_dir):
 
     df = pd.read_csv(csv_path)
     os.makedirs(output_dir, exist_ok=True)
-    my_func.exists_dir(output_dir)
+    my_func.make_dir(output_dir)
 
     for idx, row in tqdm(df.iterrows(), total=len(df)):
         # ファイル名抽出
@@ -149,7 +153,7 @@ def reverbe_only(csv_path, speech_dir, ir_dir, output_dir):
         speech_reverb = apply_ir(speech, speech_ir)
 
         # 保存
-        out_name = f"{my_func.get_fname(speech_file)[0]}.wav"
+        out_name = f"{my_func.get_file_name(speech_file)[0]}.wav"
         out_path = os.path.join(output_dir, out_name)
         save_wav(out_path, speech_reverb, sr)
         # print(f"Saved: {out_path}")
@@ -165,7 +169,7 @@ def noise_only(csv_path, speech_dir, noise_dir, ir_dir, output_dir):
 
     df = pd.read_csv(csv_path)
     os.makedirs(output_dir, exist_ok=True)
-    my_func.exists_dir(output_dir)
+    my_func.make_dir(output_dir)
 
     for idx, row in tqdm(df.iterrows(), total=len(df)):
         # ファイル名抽出
@@ -198,7 +202,7 @@ def noise_only(csv_path, speech_dir, noise_dir, ir_dir, output_dir):
         mixed = mix_snr(speech_reverb, noise_reverb, snr)
 
         # 保存
-        out_name = f"{my_func.get_fname(speech_file)[0]}_{my_func.get_fname(noise_file)[0]}_{int(snr * 10):03}dB.wav"
+        out_name = f"{my_func.get_file_name(speech_file)[0]}_{my_func.get_file_name(noise_file)[0]}_{int(snr * 10):03}dB.wav"
         out_path = os.path.join(output_dir, out_name)
         save_wav(out_path, mixed, sr)
         # print(f"Saved: {out_path}")
@@ -216,14 +220,27 @@ if __name__ == '__main__':
     test_train = "train"
     for i in range(1, 2):
         # "C:\Users\kataoka-lab\Desktop\sound_data\sample_data\speech\GNN\subset_DEMAND\condition\train\condition_1.csv"
-        csv_path = f"{const.SAMPLE_DATA_DIR}/speech/GNN/subset_DEMAND/condition/{test_train}/condition_{i}.csv"
-        speech_dir = f"{const.SAMPLE_DATA_DIR}/speech/subset_DEMAND/{test_train}"
+        # csv_path = f"{const.SAMPLE_DATA_DIR}/speech/GNN/subset_DEMAND/condition/{test_train}/condition_{i}.csv"
+        # speech_dir = f"{const.SAMPLE_DATA_DIR}/speech/subset_DEMAND/{test_train}"
 
-        ir_dir = f"{const.SAMPLE_DATA_DIR}/IR/1ch_0cm_liner/clean"
-        output_dir =  f"{const.MIX_DATA_DIR}/subset_DEMAND_1ch/condition_{i}/{test_train}/clean"
+        # ir_dir = f"{const.SAMPLE_DATA_DIR}/IR/1ch_0cm_liner/clean"
+        # output_dir =  f"{const.MIX_DATA_DIR}/subset_DEMAND_1ch/condition_{i}/{test_train}/clean"
+        # clean(csv_path, speech_dir, ir_dir, output_dir)
+
+        # # noise_dir = "C:/Users/kataoka-lab/Desktop/sound_data/sample_data/noise/DEMAND/"
+        # ir_dir = f"{const.SAMPLE_DATA_DIR}/IR/1ch_0cm_liner/reverbe_only"
+        # output_dir =  f"{const.MIX_DATA_DIR}/subset_DEMAND_1ch/condition_{i}/{test_train}/reverbe_only"
+        # reverbe_only(csv_path, speech_dir, ir_dir, output_dir)
+        
+        csv_path = f"/Users/a/Documents/sound_data/sample_data/speech/subset_DEMAND/condition_{i}.csv"
+        speech_dir = f"/Users/a/Documents/sound_data/sample_data/speech/subset_DEMAND/{test_train}"
+
+        ir_dir = f"/Users/a/Documents/sound_data/sample_data/IR/1ch_0cm_liner/clean"
+        output_dir =  f"/Users/a/Documents/sound_data/mix_data/subset_DEMAND_1ch/condition_{i}/{test_train}/clean"
         clean(csv_path, speech_dir, ir_dir, output_dir)
 
-        # noise_dir = "C:/Users/kataoka-lab/Desktop/sound_data/sample_data/noise/DEMAND/"
-        ir_dir = f"{const.SAMPLE_DATA_DIR}/IR/1ch_0cm_liner/reverbe_only"
-        output_dir =  f"{const.MIX_DATA_DIR}/subset_DEMAND_1ch/condition_{i}/{test_train}/reverbe_only"
-        reverbe_only(csv_path, speech_dir, ir_dir, output_dir)
+        noise_dir = "/Users/a/Documents/sound_data/sample_data/noise/hoth.wav"
+        ir_dir = f"/Users/a/Documents/sound_data/sample_data/IR/1ch_0cm_liner/reverbe_only"
+        output_dir =  f"/Users/a/Documents/sound_data/mix_data/subset_DEMAND_1ch/condition_{i}/{test_train}/noise_reverbe"
+        noise_reverbe(csv_path, speech_dir, noise_dir, ir_dir, output_dir)
+        
