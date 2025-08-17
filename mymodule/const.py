efe5a907cb5f4dad06cd584c7561097d091a592d
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-08-17 16:43:50 +0900

    `const.py`の文字列フォーマットを統一し、Windows/Mac間のパス設定を調整

--- mymodule/const.py
+++ mymodule/const.py
@@ -3,31 +3,33 @@ construction
 設定
 
 """
+
 import os.path
 
-DATASET_KEY_TARGET = 'target'
-DATASET_KEY_MIXDOWN = 'mix'
+DATASET_KEY_TARGET = "target"
+DATASET_KEY_MIXDOWN = "mix"
 
-DIR_KEY_TARGET = 'target'
-DIR_KEY_NOISE = 'noise'
-DIR_KEY_MIX = 'mix'
+DIR_KEY_TARGET = "target"
+DIR_KEY_NOISE = "noise"
+DIR_KEY_MIX = "mix"
 
-SAUND_DATA_DIR = 'C:\\Users\\kataoka-lab\\Desktop\\sound_data\\'
-SAMPLE_DATA_DIR = os.path.join(SAUND_DATA_DIR, 'sample_data')
-MIX_DATA_DIR = os.path.join(SAUND_DATA_DIR, 'mix_data')
-DATASET_DIR = os.path.join(SAUND_DATA_DIR, 'dataset')
+# SAUND_DATA_DIR = 'C:\\Users\\kataoka-lab\\Desktop\\sound_data\\'    # Windows用
+SAUND_DATA_DIR = "/Users/a/Documents/sound_data/"  # Mac用
+SAMPLE_DATA_DIR = os.path.join(SAUND_DATA_DIR, "sample_data")
+MIX_DATA_DIR = os.path.join(SAUND_DATA_DIR, "mix_data")
+DATASET_DIR = os.path.join(SAUND_DATA_DIR, "dataset")
 
-RESULT_DIR = 'C:\\Users\\kataoka-lab\\Desktop\\sound_data\\RESULT\\'
-OUTPUT_WAV_DIR = os.path.join(RESULT_DIR, 'output_wav')
-LOG_DIR = os.path.join(RESULT_DIR, 'log')
-PTH_DIR = os.path.join(RESULT_DIR, 'pth')
-EVALUATION_DIR = os.path.join(RESULT_DIR, 'evaluation')
+RESULT_DIR = os.path.join(SAUND_DATA_DIR, "RESULT")
+OUTPUT_WAV_DIR = os.path.join(RESULT_DIR, "output_wav")
+LOG_DIR = os.path.join(RESULT_DIR, "log")
+PTH_DIR = os.path.join(RESULT_DIR, "pth")
+EVALUATION_DIR = os.path.join(RESULT_DIR, "evaluation")
 
 
 SR = 16000  # サンプリング周波数
-FFT_SIZE = 1024 # FFTのサイズ
-H = 256 # 窓長
+FFT_SIZE = 1024  # FFTのサイズ
+H = 256  # 窓長
 
 BATCHSIZE = 1  # バッチサイズ
-PATCHLEN = 16   # パッチサイズ
-EPOCH = 100 # 学習回数
+PATCHLEN = 16  # パッチサイズ
+EPOCH = 100  # 学習回数
