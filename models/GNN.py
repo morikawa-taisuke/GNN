ffa795f621acc767de7bdeb7590767e85f05702e
morikawa-taisuke <y24m019@mail.ryukoku.ac.jp>
2025-08-21 21:57:28 +0900

    `GNN.py`: 不使用の`create_knn_graph`メソッドを削除し、コードを簡素化

--- models/GNN.py
+++ models/GNN.py
@@ -158,11 +158,6 @@ class UGNN(nn.Module):
 
         self.graph_builder = GraphBuilder(graph_config)
 
-    def create_knn_graph(self, x_nodes_batched, k, batch_size, num_nodes_per_sample):
-        batch_indices = torch.arange(batch_size, device=x_nodes_batched.device).repeat_interleave(num_nodes_per_sample)
-        edge_index = knn_graph(x=x_nodes_batched, k=k, batch=batch_indices, loop=False)
-        return edge_index
-
     def forward(self, x):
         # エンコーダ
         x_encoded = self.encoder(x)
